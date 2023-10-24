97de6d8028ee599f034e26954f511d49b7c4d99c
Malcolm Seyd <malcolm@seyd.ca>
2023-10-23 23:51:44 -0700

    simplify database config
    neon.tech finally released a good Ecto guide so I just copied it

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -1,16 +1,14 @@
 import Config
 
 config :backend, Backend.Repo,
-  url: System.get_env("DB_URL"),
-  hostname: System.get_env("DB_HOST", ""),
+  database: System.get_env("DB_NAME"),
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASS"),
+  hostname: System.get_env("DB_HOST"),
   ssl: true,
   ssl_opts: [
+    server_name_indication: to_charlist(System.get_env("DB_HOST")),
     verify: :verify_peer,
     cacertfile: "/etc/ssl/certs/ca-certificates.crt",
-    server_name_indication: String.to_charlist(System.get_env("DB_HOST", "")),
-    customize_hostname_check: [match_fun: :public_key.pkix_verify_hostname_match_fun(:https)],
-    depth: 3
-    # verify_fun:
-    #   {&:ssl_verify_hostname.verify_fun/3,
-    #    [check_hostname: String.to_charlist(System.get_env("DB_HOST", ""))]}
+    customize_hostname_check: [match_fun: :public_key.pkix_verify_hostname_match_fun(:https)]
   ]
